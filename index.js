11e1a83b4506028f75c6f1f490b583bf79719a4a
Tebogo Mosiane <mosianets@gmail.com>
2024-03-19 15:36:54 +0200

    Created ternary operators for all challenges

---
+++ index.js
@@ -0,0 +1,54 @@
+// # [JSL04] Submission: Monster Ternary Operator
+
+// #### Challenge 1: Flavor Selection
+// Given a variable that holds a user's preferred type of Monster Energy drink, 
+// print whether the user prefers a "Regular" or "Sugar-free" variety. If the preference is not set, default to "Regular".
+
+let userPreference = 'Sugar-free'; // Possible values: 'Regular', 'Sugar-free', or undefined
+let typeDecision = (userPreference == 'Regular') ? "Regular" : (userPreference == 'Sugar-free') ? "Sugar-free" : "Regular"
+console.log(`This guy's preferred type is ${typeDecision}`);
+
+// #### Challenge 2: Stock Check
+// You have a variable that tracks the number of Monster Energy cans left in the fridge. 
+// Use a ternary operator to print "Time to restock!" if the number is less than 5, or "We're stocked!" if the number is 5 or more.
+
+let cansLeft = 5; // Any number of cans
+let stockDecision =  (cansLeft < 5) ? 'Time to restock' : 'We are stocked'
+console.log(stockDecision)
+
+// #### Challenge 3: Workout Intensity
+// Based on the current heart rate, determine if the user should drink a Monster Energy drink to boost their workout. 
+// If the heart rate is below 100 bpm, print "Boost needed!", otherwise, print "Energy levels are high!".
+
+let heartRate = 120; // Current heart rate in bpm
+let drinkDecision =  (heartRate < 100) ? 'Boost needed' : 'Energy levels are high'
+console.log(drinkDecision)
+
+
+// #### Challenge 4: Temperature Suitability
+// Monster Energy drinks are best served cold. Given the current temperature, use a ternary to print "Chilled to perfection!" 
+// if the temperature is below or equal to 5°C, or "Needs a cooler!" if above 5°C.
+
+let currentTemp = 5; // Current temperature in °C
+let tempDecision = (currentTemp > 5) ? 'Needs a cooler': 'Chilled to perfection'
+console.log(tempDecision);
+
+// #### Challenge 5: Late Night Coding Session
+// Determine if it's a good idea to have a Monster Energy drink based on the current hour.
+// If it's between 7am and midnight (24-hour format), print "Unleash the beast!",
+// otherwise, print "Better stick to water."
+
+// Define the current hour in 24-hour format
+let currentHour = 15; 
+let hourDecision = (currentHour > 7 && currentHour < 24) ? 'Unleash the beast': 'Better stick to water'
+console.log(hourDecision)
+
+// Write your code below this line to determine if it's a good idea to have a Monster Energy drink based on the current hour
+// If it's between 7am and midnight (24-hour format), print "Unleash the beast!"
+// Otherwise, print "Better stick to water."
+
+
+
+
+
+
